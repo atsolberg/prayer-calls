68d54e91eb2c0b503618831a3de6572139f048a7
atsolberg <atsolberg@gmail.com>
2022-03-14 16:50:20 -0500

    updated header and footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,19 @@
 function Footer() {
-  return <footer>footer</footer>;
+  return (
+    <footer className="text-sm leading-6 mt-12">
+      <div className="p-2 sm:p-10 border-t border-slate-200 sm:flex justify-between text-slate-500 dark:border-slate-200/5">
+        <div className="mb-6 sm:mb-0 sm:flex">
+          <p>Copyright © 2022 Solberg LLC.</p>
+        </div>
+        <a
+          className="hover:text-slate-900 dark:hover:text-slate-400"
+          href="https://github.com/atsolberg/prayer-calls"
+        >
+          Source on GitHub
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
