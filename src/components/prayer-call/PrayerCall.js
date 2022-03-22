88a20989d36d5df9329ccc1f272151352e057656
atsolberg <atsolberg@gmail.com>
2022-03-22 17:10:40 -0500

    added links for verses

--- src/components/prayer-call/PrayerCall.js
+++ src/components/prayer-call/PrayerCall.js
@@ -25,39 +25,46 @@ function Verse(props) {
   const [bibleId] = useBibles();
   const verse = props.verse.replace(/_/g, "");
   const [scripture, setScripture] = useState("");
+  const isRange = verse.includes("-");
+  const end = verse.lastIndexOf(" ");
+  const name = verse.substring(0, end).trim();
+  const colon = verse.indexOf(":");
+  const chapter = verse.substring(end, colon).trim();
+  const book = books.find((b) => b.name === name);
+  const v = verse.substring(
+    colon + 1,
+    isRange ? verse.indexOf("-") : undefined
+  );
+  let verseId = `${book?.id}.${chapter}.${v}`;
+  if (isRange) {
+    const lv = verse.substring(verse.indexOf("-") + 1);
+    verseId = `${verseId}-${book?.id}.${chapter}.${lv}`;
+  }
 
   useEffect(() => {
-    const isRange = verse.includes("-");
-    const end = verse.lastIndexOf(" ");
-    const name = verse.substring(0, end).trim();
-    const colon = verse.indexOf(":");
-    const chapter = verse.substring(end, colon).trim();
-    const v = verse.substring(
-      colon + 1,
-      isRange ? verse.indexOf("-") : undefined
-    );
-    const book = books.find((b) => b.name === name);
     if (!book) {
       console.log("Invalid book name", name);
     } else {
-      let verseId = `${book.id}.${chapter}.${v}`;
-      if (isRange) {
-        const lv = verse.substring(verse.indexOf("-") + 1);
-        verseId = `${verseId}-${book.id}.${chapter}.${lv}`;
-      }
       getVerse(bibleId, verseId)
-        .then((resp) => {
-          setScripture(resp.content);
-        })
+        .then((resp) => setScripture(resp.content))
         .catch((err) => {
           console.log(`Error fetching verse id "${verseId}"`, err);
         });
     }
-  }, [bibleId, verse]);
+  }, [bibleId, book, name, verseId]);
 
   return (
     <div className="verse">
-      <div className="verse text-sky-500 dark:text-sky-400 py-1">{verse}</div>
+      <p className="py-1">
+        <a
+          href={`https://my.bible.com/bible/1/${book?.id}.${chapter}.KJV`}
+          className="text-sky-500 dark:text-sky-400"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {verse}
+        </a>
+      </p>
       <blockquote
         css={vStyles}
         className="m-2 p-4 inset-0 dark:bg-slate-700/25 rounded"
